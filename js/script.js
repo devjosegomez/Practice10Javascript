b2ff9410cdb17372edf6fc17d8688035e1590fed
devjosegomez <devjosegomez@gmail.com>
2019-10-29 12:14:01 -0600

    Primitive types

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//Week 5 cohort 18 Generation
+const PI_NUMBER = 3.1416;
+
+let generation = 18;
+let graduated = false;
+let ticket = 9.00;
+let grade = 'A';
+
+let undefinedVar = undefined;
+let nanVar = NaN;
+let wayBehind = -Infinity;
+let wayFar = Infinity;
+
+let number1 = Number(prompt("Enter a number 1:"));
+let number2 = Number(prompt("Enter a number 2:"));
+
+console.log(number1 + "+" + number2 + "=" + (number1+number2));
+console.log("Generation Cohort 18");
